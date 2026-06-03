d925d45889bc06d80f28c7e1e3a4ddb0a2d2167f
karim <hema_thechamp@yahoo.com>
2026-06-03 17:05:29 -0400

    Filter model dropdown by selecetd make

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const yearSelect = document.querySelector("#year");
 const makeSelect = document.querySelector("#make");
+const modelSelect = document.querySelector("#model");
 
 function getUniqueValues(data, key) {
   const values = data.map((item) => item[key]);
@@ -30,10 +31,13 @@ function addOptions(selectElement, options) {
 const years = getUniqueValues(partsData, "year");
 
 makeSelect.disabled = true;
+modelSelect.disabled = true;
 addOptions(yearSelect, years);
 
 yearSelect.addEventListener("change", () => {
   resetSelect(makeSelect, "Select make");
+  resetSelect(modelSelect, "Select model");
+  modelSelect.disabled = true;
 
   if (yearSelect.value === "") {
     makeSelect.disabled = true;
@@ -45,4 +49,21 @@ yearSelect.addEventListener("change", () => {
 
   addOptions(makeSelect, makes);
   makeSelect.disabled = false;
+});
+
+makeSelect.addEventListener("change", () => {
+  resetSelect(modelSelect, "Select model");
+
+  if (makeSelect.value === "") {
+    modelSelect.disabled = true;
+    return;
+  }
+
+  const matchingRows = partsData.filter((part) => {
+    return part.year === yearSelect.value && part.make === makeSelect.value;
+  });
+  const models = getUniqueValues(matchingRows, "model");
+
+  addOptions(modelSelect, models);
+  modelSelect.disabled = false;
 });
