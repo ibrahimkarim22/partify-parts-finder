2c160c9708e57e348862fc781b15bce9999ce651
karim <hema_thechamp@yahoo.com>
2026-06-03 16:23:54 -0400

    filter make dropdown by selected year

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const yearSelect = document.querySelector("#year");
+const makeSelect = document.querySelector("#make");
 
 function getUniqueValues(data, key) {
   const values = data.map((item) => item[key]);
   return [...new Set(values)].sort();
+}
+
+function resetSelect(selectElement, placeholderText) {
+  selectElement.innerHTML = "";
+
+  const placeholderOption = document.createElement("option");
+  placeholderOption.value = "";
+  placeholderOption.textContent = placeholderText;
+
+  selectElement.appendChild(placeholderOption);
 }
 
 function addOptions(selectElement, options) {
@@ -18,4 +29,20 @@ function addOptions(selectElement, options) {
 
 const years = getUniqueValues(partsData, "year");
 
+makeSelect.disabled = true;
 addOptions(yearSelect, years);
+
+yearSelect.addEventListener("change", () => {
+  resetSelect(makeSelect, "Select make");
+
+  if (yearSelect.value === "") {
+    makeSelect.disabled = true;
+    return;
+  }
+
+  const matchingRows = partsData.filter((part) => part.year === yearSelect.value);
+  const makes = getUniqueValues(matchingRows, "make");
+
+  addOptions(makeSelect, makes);
+  makeSelect.disabled = false;
+});
